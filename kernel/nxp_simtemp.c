9ab04d62bf451fb990e03347eb244a59f8859ea8
albert <alberto.31roo@gmail.com>
2025-10-28 17:08:49 -0600

    Char device, pollin and read functions were added

--- kernel/nxp_simtemp.c
+++ kernel/nxp_simtemp.c
@@ -7,6 +7,7 @@
 #include <linux/hrtimer.h>
 #include <linux/workqueue.h>
 #include <linux/ktime.h>
+#include <linux/poll.h>
 #include "nxp_simtemp.h"
 
 
@@ -24,7 +25,14 @@ struct simtemp_drv{
     struct simtemp_sample *data;
 };
 
-__u64 time_sample;    // Time for each temperature sample
+/* Time for each temperature sample */
+__u64 time_sample;    
+__s32 temp_alert = ALERT_TEMP;
+
+/* Variables to create char device and file to manipulate it*/
+static int chrdev_ret;
+static struct class *simtemp_class;
+static struct device *simtemp_device;
 
 /*Global pointer for acess driver*/
 static struct simtemp_drv *global_simtemp = NULL;
@@ -44,22 +52,29 @@ static struct work_struct temperaure_work;
 /*Variable to validate if temperatures was read*/
 bool temp_done = true;
 
+/* Init variables for poll*/
+static DECLARE_WAIT_QUEUE_HEAD(simtemp_qwait);
+static DEFINE_MUTEX(simtemp_lock);
+
 /*Function to be run in the queue*/
-static void temperature_read(struct work_struct *work)
+static void temperature_update(struct work_struct *work)
 {
-    ktime_t time_read = ktime_get();
+    ktime_t time_read = ktime_get_real();
 
     /* Update data data for sensor */
     global_simtemp->data->temp_mC += (__s32)TEMP_STEP;
     global_simtemp->data->timestamp_ns = ktime_to_ns(time_read);
     global_simtemp->data->flags |= NEW_SAMPLE_MASK; 
 
+    /* Wake poll function, new sample is ready*/
+    wake_up_interruptible(&simtemp_qwait);
+
     /* Set flag if temperature is above 50° */
-    if(ALERT_TEMP < global_simtemp->data->temp_mC)
+    if(temp_alert < global_simtemp->data->temp_mC)
     {
         global_simtemp->data->flags |= TEMP_ALERT_MASK; 
 
-        /* Reset temperature to avoid burning*/
+        /* Reset temperature to avoid burning something */
         if(RESET_TEMP < global_simtemp->data->temp_mC)
         {
             global_simtemp->data->temp_mC = (__s32)CERO_DEGREE;
@@ -93,6 +108,70 @@ static enum hrtimer_restart temp_timer_callback(struct hrtimer *timer)
     return HRTIMER_RESTART;
 }
 
+static unsigned int simtemp_poll(struct file *file, poll_table *wait)
+{
+    unsigned int simtemp_mask = 0;
+
+    /* Set qwait to the queue*/
+    poll_wait(file, &simtemp_qwait, wait);
+
+    /* Validate that data is available */
+    if(global_simtemp->data->flags & NEW_SAMPLE_MASK)
+    {
+        simtemp_mask = POLLIN; 
+    }
+
+    return simtemp_mask;
+}
+
+
+static ssize_t simtemp_read(struct file *file, char __user *buf, size_t count, loff_t *ppos)
+{
+    /* Copy of data to be used on user space*/
+    struct simtemp_sample sample_userspace;
+    ssize_t count_data;
+
+    /* Sleep function until new sample */
+    if(wait_event_interruptible(simtemp_qwait, global_simtemp->data->flags & NEW_SAMPLE_MASK))
+    {
+        return -ERESTARTSYS;
+    }
+        
+    /* Lock code for other users until read function detects a new sample*/    
+    mutex_lock(&simtemp_lock);
+
+    sample_userspace = *global_simtemp->data;
+
+    /* Clear new sample flag */
+    global_simtemp->data->flags &= ~NEW_SAMPLE_MASK;
+
+    /* Unlock code for other user until read function detects a new sample*/    
+    mutex_unlock(&simtemp_lock);
+
+    /* Validate size of buf using count*/
+    if(count < sizeof(sample_userspace))
+    {
+        return -EINVAL;
+    }
+
+    /* Copy data to user space */
+    if(copy_to_user(buf, &sample_userspace, sizeof(sample_userspace)))
+    {
+        return -EFAULT;
+    }
+
+    count_data = sizeof(sample_userspace);
+
+    return count_data;
+}
+
+
+static const struct file_operations simtemp_fops = {
+    .owner = THIS_MODULE,
+    .read = simtemp_read,
+    .poll = simtemp_poll,
+};
+
 /*Probe function*/
 static int simtemp_probe(struct platform_device *client) 
 {
@@ -119,7 +198,7 @@ static int simtemp_probe(struct platform_device *client)
     /*Create the queue list*/
     simtemp_queue = create_singlethread_workqueue("simtemp_queue");
     /*Assignt function to struct*/
-    INIT_WORK(&temperaure_work, temperature_read);
+    INIT_WORK(&temperaure_work, temperature_update);
 
     /*Assignt value to counter variable*/
     time_sample = ktime_set(0, INIT_TIME);
@@ -173,17 +252,47 @@ static int __init simtemp_init(void)
         pr_err("Driver could not be registered\n");
         return ret;
     }
-    else
-    {
-        /* Register a platform device to use probe,remove because  VirtualBox/QEMU can not use dts*/
-        tmp_simtemp = platform_device_register_simple("simtemp", -1, NULL, 0);
-        if(IS_ERR(tmp_simtemp)) 
-        {
-            pr_err("Device could not be registered\n");
-            platform_driver_unregister(&simtemp_driver);
-            return PTR_ERR(tmp_simtemp);
-        }
 
+    /* Register a platform device to use probe,remove because  VirtualBox/QEMU can not use dts*/
+    tmp_simtemp = platform_device_register_simple("simtemp", -1, NULL, 0);
+    if(IS_ERR(tmp_simtemp)) 
+    {
+        pr_err("Device could not be registered\n");
+        platform_driver_unregister(&simtemp_driver);
+        return PTR_ERR(tmp_simtemp);
+    }
+
+    /* Register a char device to be used in user space*/
+    chrdev_ret = register_chrdev(0, "simtemp", &simtemp_fops);
+    if(chrdev_ret < 0) 
+    {
+        pr_err("Error during device char register\n");
+        platform_device_unregister(tmp_simtemp);
+        platform_driver_unregister(&simtemp_driver);
+        return chrdev_ret;
+    }
+
+    /* Create class for device */
+    simtemp_class = class_create("simtemp_class");
+    if(IS_ERR(simtemp_class)) 
+    {
+        platform_device_unregister(tmp_simtemp);
+        platform_driver_unregister(&simtemp_driver);
+        unregister_chrdev(chrdev_ret, "simtemp");
+        pr_err("Error to create the device \n");
+        return PTR_ERR(simtemp_class);
+    }
+
+    /* Create node for /dev/simtemp */
+    simtemp_device = device_create(simtemp_class, NULL, MKDEV(chrdev_ret, 0), NULL, "simtemp");
+    if(IS_ERR(simtemp_device)) 
+    {
+        platform_device_unregister(tmp_simtemp);
+        platform_driver_unregister(&simtemp_driver);
+        unregister_chrdev(chrdev_ret, "simtemp");
+        class_destroy(simtemp_class);
+        pr_err("Error to create the device\n");
+        return PTR_ERR(simtemp_device);
     }
 
     return 0;
@@ -193,15 +302,20 @@ static int __init simtemp_init(void)
 static void __exit simtemp_exit(void)
 {
     /*Delete timer*/
-    int ret = hrtimer_cancel(&temp_timer);
-    pr_info("hrtimer cancelado (%d)\n", ret);
+    hrtimer_cancel(&temp_timer);
     /*Clear works in the queue*/
     flush_workqueue(simtemp_queue); 
     /*Delete queue*/
     destroy_workqueue(simtemp_queue);
-
-    pr_info("Module removed\n");
+    /*Delete device file*/
+    device_destroy(simtemp_class, MKDEV(chrdev_ret, 0));
+    /*Delete class of device*/
+    class_destroy(simtemp_class);
+    /*Delete char device*/
+    unregister_chrdev(chrdev_ret, "simtemp");
+    /*Delete device*/
     platform_device_unregister(tmp_simtemp);
+    /*Delete driver*/
     platform_driver_unregister(&simtemp_driver);
 }
 
